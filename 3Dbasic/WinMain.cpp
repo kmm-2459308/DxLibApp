e4829db7085e0d1eba8401c2ed16280e6ebdcede
kmm-2459308 <kmm2459308@stu.o-hara.ac.jp>
2025-01-27 12:37:56 +0900

    Winmain

--- 3Dbasic/WinMain.cpp
+++ 3Dbasic/WinMain.cpp
@@ -2,9 +2,19 @@
 
 int WINAPI WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR IpC, int nC)
 {
+	int model1;
+	VECTOR pos = VGet(600.0f, 300.0f, -400.0f);
+	int key;
+
 	ChangeWindowMode(TRUE);
 	SetGraphMode(1200, 800, 32);
 	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+	//　モデルの読み込み
+	model1 = MV1LoadModel("Player/PC.mv1");
+	if (model1 == -1)
 	{
 		return -1;
 	}
@@ -12,8 +22,18 @@ int WINAPI WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR IpC, int nC)
 
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
+		//キー操作
+		key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+		if (key & PAD_INPUT_DOWN)pos.z -= 4.0f;
+		if (key & PAD_INPUT_UP)pos.z += 4.0f;
+		if (key & PAD_INPUT_LEFT)pos.x -= 4.0f;
+		if (key & PAD_INPUT_RIGHT)pos.x += 4.0f;
+
 		ClearDrawScreen();
-		DrawBox(0, 0, 600, 400, GetColor(255, 255, 0), TRUE);
+		DrawBox(0, 0, 1200, 800, GetColor(255, 255, 255), TRUE);
+
+		MV1SetPosition(model1, pos);
+		MV1DrawModel(model1);
 
 		ScreenFlip();
 	}
